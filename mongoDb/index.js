e08576c63d57b5193649dfc6049f42cf282cc438
Rahul <rahulvaidya148@gmail.com>
2022-06-21 16:30:45 +0530

    Read Data from MongoDb

--- mongoDb/index.js
+++ mongoDb/index.js
@@ -1,9 +1,24 @@
-const express  = require('express');
+const { MongoClient } = require("mongodb");
+const url = "mongodb://localhost:27017";
+const express = require("express");
 
 const app = express();
+const database = "e-comm";
+const client = new MongoClient(url);
+
+async function getData() {
+  let result = await client.connect();
+  let db = result.db(database);
+  let collection = db.collection("products");
+  let response = await collection.find({}).toArray();
+  console.log(response);
+}
+
+getData();
 
 
-
-
+app.get('',(req,res) => {
+    res.send(`<h1> This is Home Page</h1>`)
+})
 
 app.listen(8000);
