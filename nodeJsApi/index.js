7b8bebb656bb023205b640e6bbed1c950acb5921
Rahul <rahulvaidya148@gmail.com>
2022-06-23 15:38:23 +0530

    Created Post API method

--- nodeJsApi/index.js
+++ nodeJsApi/index.js
@@ -2,11 +2,28 @@ const express = require('express')
 const dbConnect = require('./mongodb');
 
 const app = express();
-// Api for get Data
+
+
+// middleware for Post method data i.e getdata from request or postman
+
+
+app.use(express.json());
+// Api for get Method Data
 app.get('/', async (req , res)=>{
     let data = await dbConnect();
     data =  await data.find({}).toArray();
     res.send(data)
 })
+
+
+// Api for Post Method Data
+app.post('/' , async (req,res) => {
+    console.log(req.body)
+    let data = await dbConnect();
+    let result  = await data.insertMany(req.body)
+    res.send(req.body)
+
+})
+
 
 app.listen(8000);
