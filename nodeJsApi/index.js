2b27a7cbea04b7027b6fea966771678c9bd2c8a2
Rahul <rahulvaidya148@gmail.com>
2022-06-24 16:02:03 +0530

    commit to delete

--- nodeJsApi/index.js
+++ nodeJsApi/index.js
@@ -1,44 +1,47 @@
-const express = require('express')
-const dbConnect = require('./mongodb');
-
+const express = require("express");
+const dbConnect = require("./mongodb");
+const mongodb = require("mongodb"); // makes object is gets from database
 const app = express();
-
 
 // middleware for Post method data i.e getdata from request or postman
 
-
 app.use(express.json());
 // Api for get Method Data
-app.get('/', async (req , res)=>{
-    let data = await dbConnect();
-    data =  await data.find({}).toArray();
-    res.send(data)
-})
-
+app.get("/", async (req, res) => {
+  let data = await dbConnect();
+  data = await data.find({}).toArray();
+  res.send(data);
+});
 
 // Api for Post Method Data
-app.post('/' , async (req,res) => {
-    console.log(req.body)
-    let data = await dbConnect();
-    let result  = await data.insertMany(req.body)
-    res.send(req.body)
-
-})
-
+app.post("/", async (req, res) => {
+  console.log(req.body);
+  let data = await dbConnect();
+  let result = await data.insertOne(req.body);
+  res.send(req.body);
+});
 
 // API for Update()
 
+app.put("/:name", async (req, res) => {
+  let data = await dbConnect();
+  let result = data.update(
+    { name: req.params.name }, //match data from params
+    { $set: req.body } //update data after getting from body
+  );
 
-app.put('/:name', async(req,res) => {
-    let data = await dbConnect();
-    let result = data.update(
-        {name : req.params.name} , //match data from params
-        { $set : req.body } //update data after getting from body
-      
-    )
-    
-    res.send({result :"updated"})
-})
+  res.send({ result: "updated" });
+});
 
+// API for delete
+
+app.delete("/:id", async (req, res) => {
+  const data = await dbConnect();
+  //   console.log(req.params.id);
+  const result = await data.deleteOne({
+    _id: new mongodb.ObjectId(req.params.id)
+  });
+  res.send("delete");
+});
 
 app.listen(8000);
