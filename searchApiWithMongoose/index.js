9ed935707d8e6f7107021b10c64559265f2babe5
Rahul <rahulvaidya148@gmail.com>
2022-06-25 09:27:57 +0530

    commit search Api

--- searchApiWithMongoose/index.js
+++ searchApiWithMongoose/index.js
@@ -1,12 +1,20 @@
-const mongoose = require('mongoose');
-const express = require('express');
-require('./config')
-const Product = require('./product');
-
+const mongoose = require("mongoose");
+const express = require("express");
+require("./config");
+const Product = require("./product");
 
 // makes executable to express
 const app = express();
 
-
+app.get("/search/:key", async (req, res) => {
+  let data = await Product.find({
+    $or: [
+      { name: { $regex: req.params.key } },
+      { brand: { $regex: req.params.key } },
+      { category: { $regex: req.params.key } },
+    ],
+  });
+  res.send(data);
+});
 // listen at port 5000
-app.listen(5000)
+app.listen(5000);
